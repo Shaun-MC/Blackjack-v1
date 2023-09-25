bab71910aaac2310381adf02a5a6688c6c3ecaae
Shaun <you@example.com>
2023-09-25 12:21:42 -0700

    ...

--- genTester.cpp
+++ genTester.cpp
@@ -9,25 +9,6 @@
 using namespace std;
 
 int main() {
-    
-    Dealer dealerInstance;
-    Deck deckInstance;
-
-    deckInstance.initializeDeck(0,0,0);
-
-    int handArr[2];
-
-    for (int i = 0; i < 3; i++){
-            
-            deckInstance.getCard(&handArr[0]);
-            dealerInstance.addToHand(handArr[0], handArr[1]);
-    }
-
-    cout << "DEALER UP CARD: ";
-
-    dealerInstance.printCard();
-
-    dealerInstance.displayHand();
 
     return 0;
 }
