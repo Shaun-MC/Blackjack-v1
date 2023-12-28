2bd89118204eaf613929704d62af7655f68e881a
Shaun-MC <shaunmcushman@gmail.com>
2023-12-28 13:07:59 -0800

    developed desired functions, small fixes in other classes

--- Controlled_Player.cpp
+++ Controlled_Player.cpp
@@ -6,15 +6,20 @@ Controlled_Player::Controlled_Player() {
     Hand temp;
 
     this->hands_.push_back(temp);
+}
 
-    this->stats; // Investigate nessessity 
+int Controlled_Player::hand_total(const int hand_index) const { // UNTESTED
+
+    const int total1 = this->hands_[hand_index].hand_totals1();
+    const int total2 = this->hands_[hand_index].hand_totals0();
+    
+    return (total1 > total2) ? total1 : total2;
 }
 
 // Actions
+void Controlled_Player::ReceiveCard(const Card& new_card, const int hand_index) {
 
-void ReceiveCard(const Card& new_card) {
-
-    return;
+    this->hands_[hand_index].AddCardToHand(new_card);
 }
 
 bool Controlled_Player::NaturalsCheck(const int hand_index) const {
@@ -32,6 +37,11 @@ bool Controlled_Player::BustCheck(const int hand_index) const {
     return (this->hands_[hand_index].hand_totals0() >= 22) ? true : false;
 }
 
+bool Controlled_Player::SplitCheck(const int hand_index) const {
+
+    return (this->hands_[hand_index].front_card() == this->hands_[hand_index].back_card());
+}
+
 void Controlled_Player::BeginningBalance(const int balance) {
 
     this->stats.set_balances(balance);
@@ -39,7 +49,7 @@ void Controlled_Player::BeginningBalance(const int balance) {
 
 int Controlled_Player::Balance() const {
 
-    this->stats.current_balance();
+    return this->stats.current_balance();
 }
 
 void Controlled_Player::PlaceBet(const int bet) {
@@ -50,6 +60,45 @@ void Controlled_Player::PlaceBet(const int bet) {
 int Controlled_Player::Bet() const {
 
     return this->stats.current_bet();
+}
+
+bool Controlled_Player::HandBusted(const int hand_key) const {
+
+    return (this->hands_[hand_key].hand_totals0() > 21);
+}
+
+void Controlled_Player::AddAHand(const Card& split_card) { // UNTESTED
+
+    Hand temp;
+
+    this->hands_.push_back(temp);
+
+    this->hands_[this->hands_.size() - 1].AddCardToHand(split_card);
+}
+
+void Controlled_Player::DisplayHand(const int hand_key) const {
+
+    this->hands_[hand_key].DisplayHand();
+}
+
+void Controlled_Player::Wins(const int hand_type) {
+
+    this->stats.UpdateBalance(hand_type);
+}
+
+void Controlled_Player::Loss(const int hand_type) {
+
+    this->stats.UpdateBalance(hand_type + 2);
+}
+
+void Controlled_Player::Shove() {
+
+    this->stats.UpdateBalance(5);
+}
+
+void Controlled_Player::UpdateGameStatistics() { // More than just calling a stats function
+
+    
 }
 
 void Controlled_Player::DisplayHands() const {
@@ -65,9 +114,11 @@ void Controlled_Player::DisplayStatisitics() const {
     this->stats.DisplayGameStatisitics();
 }
 
-// Private Member Fucntions
+void Controlled_Player::FlushHands() {
 
-bool Controlled_Player::SplitCheck() const {
+    for (int i = 0; i < this->hands_.size() - 1; i++) {
 
-    return (this->hands_[0].front_card() == this->hands_[0].back_card()) ? true : false;
+        this->hands_[i].EmptyHand();
+    }
 }
+// Private Member Functions
